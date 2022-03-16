8994477c81a8385405bfd2e5dde99479acc4a4eb
Batey Josue <josuebatey19@gmail.com>
2022-03-16 08:42:12 +0200

    load static files

--- core/views.py
+++ core/views.py
@@ -3,9 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-# @login_required
-# def my_view(request):
-    
+@login_required
 def index(request):
     context = {}
-    return render(request, 'account/login.html', context)
+    return render(request, 'dashboard.html', context)
