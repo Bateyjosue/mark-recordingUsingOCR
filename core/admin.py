062130b03d92c5444b978a52cee6e5c79c6af162
Batey Josue <josuebatey19@gmail.com>
2022-03-16 16:51:12 +0200

    register model in admin dashboard

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CourseModel
 
 # Register your models here.
+
+@admin.register(CourseModel)
+class CourseModelAdmin(admin.ModelAdmin):
+    list_display = ('user','module_code', 'module_title', 'student_reg_number', 'marks', 'start_periode', 'end_periode')
+    fields = []
