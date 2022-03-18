8479d2548b730c42966545ad774c34753ea7ddee
Batey Josue <josuebatey19@gmail.com>
2022-03-18 09:37:19 +0200

    Register Module Models in django admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import CourseModel
+from .models import CourseModel, ModuleModel
 
 # Register your models here.
 
 @admin.register(CourseModel)
 class CourseModelAdmin(admin.ModelAdmin):
-    list_display = ('user','module_code', 'module_title', 'student_reg_number', 'marks', 'start_periode', 'end_periode')
+    list_display = ('user','module','status', 'student_reg_number', 'marks', 'start_periode', 'end_periode')
     fields = []
+    
+@admin.register(ModuleModel)
+class ModuleModelAdmin(admin.ModelAdmin):
+    list_display = ('module_code', 'module_title', 'module_description', 'module_credit')
