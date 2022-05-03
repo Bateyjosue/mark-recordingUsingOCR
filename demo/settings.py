fea32a20954711462ea4accbafc2c1a8f324b556
Batey Josue <josuebatey19@gmail.com>
2022-05-03 18:41:13 +0200

    Fix settings

--- demo/settings.py
+++ demo/settings.py
@@ -97,7 +97,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+        "NAME": os.path.join(os.getcwd(), 'db.sqlite3')
     }
 }
 
