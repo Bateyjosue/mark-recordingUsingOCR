8f018efdd9de262db22cc60f479e32b918b8393f
Batey Josue <josuebatey19@gmail.com>
2022-05-23 11:29:15 +0200

    register models mark

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import UpLoadImage
+from .models import UpLoadImage, Marks
 
 # Register your models here.
 
 @admin.register(UpLoadImage)
 class UpLoadImageAdmin(admin.ModelAdmin):
     list_display = ('pk','user','picture', 'add')
+
+@admin.register(Marks)
+class MarksAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'student_reg_number', 'mark')
