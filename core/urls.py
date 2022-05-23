197bf38f97ad4f12ded5096c924a8d517b047c96
Batey Josue <josuebatey19@gmail.com>
2022-05-23 11:30:14 +0200

    add then capture endpoint

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, record_mark
+from .views import index, record_mark, capture
 
 
 urlpatterns = [
     path('', index, name='home'),
-    path('record', record_mark, name='record'),
-    # path('capture', capture, name='capture')
+    # path('record', record_mark, name='record'),
+    path('capture', capture, name='capture')
     
 ]
 
