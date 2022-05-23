40fcbb6eb5431ad6e2ae8a2298e121043199ad51
Batey Josue <josuebatey19@gmail.com>
2022-05-23 11:30:31 +0200

    add capture views

--- core/views.py
+++ core/views.py
@@ -6,16 +6,24 @@ from django.core.mail import EmailMessage
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import UpLoadImage
+from .models import UpLoadImage, Marks
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 
 from .forms import ImageForm
-
+import cv2
+import requests
+from urllib.parse import urlparse
+import json
 
 
 @login_required
 def index(request):
+    # try:
+    #     cam = VideoCamera()
+    #     return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    # except :
+    #     pass
     return render(request, 'dashboard.html')
 
 
@@ -26,6 +34,7 @@ def record_mark(request):
         for img in image:
             upload = UpLoadImage.objects.create(user = request.user, picture = img)
             upload.save()
+        print(upload)
         # message  = messages.success(request, "saved")
         context ={
             # 'message': message,
@@ -38,28 +47,78 @@ def record_mark(request):
         }
         return render(request, 'record.html', context)
 
-# class Video(object):
-#     def __init__(self, request):
-#         self.video = cv2.VideoCapture(0)
-#         (self.grabbed, self.frame) = self.video.read()
-#         threading.Thread(target=self.update, args=()).start()
+class VideoCamera(object):
+    # global video = None
+    def __init__(self, request):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
 
-#     def __del__(self):
-#         self.video.release()
+    def __del__(self):
+        self.video.release()
 
-#     def get_frame(self):
-#         image = self.frame
-#         _, jpeg = cv2.imencode('.jpg', image)
-#         return jpeg.tobytes()
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
 
-#     def update(self):
-#         while True:
-#             (self.grabbed, self.frame) = self.video.read()
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
 
-# def gen(self):
-#     while True:
-#         frame = camera.get_frame()
-#         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-# def capture(request):
-#     pass
+@gzip.gzip_page
+def capture(request):
+    val = []
+    v = ''
+    cam = cv2.VideoCapture(0)
+    files = '/home/josh-ishara/Documents/Project/Memoir/mark-recordingUsingOCR/img.png'
+    while True:
+        ret, frame = cam.read()
+        if not ret: 
+            print ("Error reading")
+            break
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1)
+
+        if key == ord('s'):
+            img_write = cv2.imwrite('img.png', frame)
+            if img_write:
+                pa = cv2.imread(files)
+                print(files)
+                cv2.imshow('Show Image Camptured',pa)
+                url = 'https://pen-to-print-handwriting-ocr.p.rapidapi.com/recognize/'
+                image = urlparse(files)
+                image = image.path
+                image = image.split('/')
+                image = image[-1]
+                payload = {}
+                files = [
+                    ('srcImg', (image, open(files, 'rb'), 'image/png'))
+                ]
+                headers = {
+                    "X-RapidAPI-Key": "d5e7bd069fmsh822a3ff6828266cp131b4ejsn99a6119f46b9"
+                }
+
+                response = requests.request("POST", url = url, headers = headers, data = payload, files = files)
+                v = json.loads(response.text)
+                val.append(v['value'])
+                print(val)
+
+            else:
+                print("Error::Not Captured")
+
+        elif key == ord('q'):
+            cam.release()
+            cv2.destroyAllWindows()
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    context = {
+        'mark' : Marks.objects.all(),
+    }
+    return render(request, 'capture.html', context)
